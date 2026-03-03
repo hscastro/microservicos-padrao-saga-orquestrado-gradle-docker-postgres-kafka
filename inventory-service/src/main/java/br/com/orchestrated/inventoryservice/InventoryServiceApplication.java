05ba841bceebdff8b3118c40db22285061591a84
hacastro <halisonsc5@gmail.com>
2026-03-03 13:16:06 -0300

    Adiciona as classes microservice inventory-service

--- inventory-service/src/main/java/br/com/orchestrated/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/br/com/orchestrated/inventoryservice/InventoryServiceApplication.java
@@ -1,4 +1,4 @@
-package br.com.microservices.orchestrated.inventoryservice;
+package br.com.orchestrated.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ inventory-service/src/main/java/br/com/orchestrated/inventoryservice/core/enums/ESagaStatus.java
@@ -0,0 +1,7 @@
+package br.com.orchestrated.inventoryservice.core.enums;
+
+public enum ESagaStatus {
+    SUCCESS,
+    ROLLBACK_PENDING,
+    FAIL
+}
