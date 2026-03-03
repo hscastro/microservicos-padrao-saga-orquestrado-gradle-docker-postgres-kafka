ea89d91c82db69459821e31137e4a19f03d63aee
hacastro <halisonsc5@gmail.com>
2026-03-03 16:02:26 -0300

    Ajustes nos microservices

--- payment-service/src/main/java/br/com/orchestrated/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/br/com/orchestrated/paymentservice/PaymentServiceApplication.java
@@ -1,4 +1,4 @@
-package br.com.microservices.orchestrated.paymentservice;
+package br.com.orchestrated.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- product-validation-service/src/main/java/br/com/orchestrated/productvalidationservice/ProductValidationServiceApplication.java
+++ product-validation-service/src/main/java/br/com/orchestrated/productvalidationservice/ProductValidationServiceApplication.java
@@ -1,12 +1,13 @@
-package br.com.microservices.orchestrated.orderservice;
+package br.com.orchestrated.productvalidationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+public class ProductValidationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(ProductValidationServiceApplication.class, args);
 	}
+
 }
